825761294ec14e1c367ce4194b3e4c83f2ec4c6a
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-28 17:36:23 +0700

    Implemented functionalities to load autocomplete list on click of submit query, and implemented stylesheets on design of index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             success: function(data)
             {
                 console.log(data);
-                alert(data);
+                provideAutocomplete($("#countryInput"), data);
             },
             error: function(xhr, status, error)
             {
@@ -20,3 +20,35 @@ $(document).ready(function(){
         e.preventDefault();
     });
 });
+
+function provideAutocomplete(input, data) {
+    let a, b;
+    let val = $("#countryInput").val();
+    closeAllLists();
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    $("#countryInput").parent().append(a);
+    for (i = 0; i < data.length; i++) {
+            b = document.createElement("DIV");
+            b.innerHTML = "<strong>" + data[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += data[i].substr(val.length);
+            b.addEventListener("click", function(e) {
+                closeAllLists();
+            });
+            a.appendChild(b);
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != input) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
